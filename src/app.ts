61b32fb342ebb55be305cfc055c86184a88a80ba
shreyaNeupane <shreyaneupane49@gmail.com>
2025-06-27 13:40:02 +0545

    added usertable

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import  express, {Application , Request , Response} from "express";
-const app:Application = express()
-const PORT:number = 3000
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const PORT: number = 3000;
 
-require('./model/index.ts')
-app.get("/",(req:Request,res:Response)=>{
-  res.send("hello world")
-})
+import * as dotenv from "dotenv";
+dotenv.config();
+
+require("./database/connection");
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
 
 app.get("/about", (req: Request, res: Response) => {
   res.send("hi world");
 });
 
-app.listen(PORT , ()=>{
-    console.log("Server has started at port " ,PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server has started at port ", PORT);
+});
---
+++ src/database/models/User.ts
@@ -0,0 +1,33 @@
+
+import { 
+    Table,
+    Column,
+    Model,
+    DataType,
+  
+} from 'sequelize-typescript'
+
+@Table({
+  tableName: "users",
+  modelName: "User",
+  timestamps: true,
+})
+class User extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  declare id: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare email: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare password: string;
+}
+export default User
