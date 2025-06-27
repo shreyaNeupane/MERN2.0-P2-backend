eedc5727a21e1987f0506b4c02825e31f6c25814
shreyaNeupane <shreyaneupane49@gmail.com>
2025-06-27 15:52:58 +0545

    leanred hashing

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../database/models/User";
+import bcrypt from "bcrypt";
 
 class AuthController {
   public static async registerUser(req: Request, res: Response): Promise<void> {
@@ -14,7 +15,7 @@ class AuthController {
     await User.create({
       username,
       email,
-      password,
+      password: bcrypt.hashSync(password, 8),
     });
 
     res.status(200).json({
@@ -22,4 +23,4 @@ class AuthController {
     });
   }
 }
-export default AuthController
+export default AuthController;
